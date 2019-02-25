eed5961bcc60485004fb748254248d8b769936fe
liangqifei <1454554357@qq.com>
2019-02-26 01:50:48 +0800

    node 编写小程序服务index静态数据

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+var fs = require("fs");
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  res.send("index");
+});
+
+module.exports = router;
